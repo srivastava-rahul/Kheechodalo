a483a412123d28b49c10155b8308e301b4ce8112
Rahul Srivastava <r1812srivastava@gmail.com>
2017-07-12 11:02:30 +0530

    View Other profile by sending just email id

--- src/main/java/com/click/controller/ProfileController.java
+++ src/main/java/com/click/controller/ProfileController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.click.entity.ProfileSetting;
 import com.click.service.ProfileSettingService;
 import com.click.utils.SecurityLibrary;
+import com.click.utils.StringUtils;
 
 @Controller
 @RequestMapping(value = "/user")
@@ -34,6 +36,40 @@ public class ProfileController {
 				profileSetting = new ProfileSetting();
 			}
 			model.addAttribute("profileSetting", profileSetting);
+				
+		 }catch( Exception e){
+			 e.printStackTrace();
+			 model.addAttribute("error", "Invalid Data ");
+		 }
+		 
+		return "profile";
+	}
+	
+	@RequestMapping(value = "/viewProfile", method = RequestMethod.GET)
+	public String viewOtherProfile( @RequestParam String email_id ,Model model) {
+		 try{
+			 if(StringUtils.checkString(email_id).length() > 0)
+			 {
+		        ProfileSetting profileSetting =  profileSettingService.findByEmailId(email_id);
+		        
+		        if(profileSetting.isPhone_flage()==true)
+		        {
+		        	profileSetting.setPhone(0);
+		        }
+		        if(profileSetting.isPaytm_flage()==true)
+		        {
+		        	profileSetting.setPaytm("Hidden");
+		        }
+		        
+		        if(profileSetting.isAddress_flage()==true)
+		        {
+		        	profileSetting.setAddress("Hidden");
+		        }
+		        
+		        
+		        model.addAttribute("profileSetting", profileSetting);
+			
+			 }
 				
 		 }catch( Exception e){
 			 e.printStackTrace();
